91238d60f6f7179400254832cd39a77c2f37fab6
SohelIslamImran <sohelislamimran@gmail.com>
2023-02-19 22:54:41 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
-const sliderCard = document.getElementById("slider-card");
-const dots = document.querySelectorAll('.slider-indicators li');
+// @ts-nocheck
 
+// const sliderCards = document.querySelectorAll(".slider-inner .slider-card");
+// const dots = document.querySelectorAll(".slider-indicators li");
+// const indicators = document.getElementById("slider-indicators");
 
-document.querySelector('.carousel-indicators').addEventListener('click', function (e) {
-    var target = [].slice.call(e.target.parentNode.children).indexOf(e.target);
-    if (target !== currentItem && target < dots.length) {
-        goToItem(target);
-    }
+// indicators?.addEventListener("click", (event) => {
+//   if (event.target.tagName !== "LI") return;
+//   const clickedDot = event.target;
+
+//   const isActiveDot =
+//     clickedDot.classList.contains("active") || clickedDot.closest(".active");
+
+//   if (!isActiveDot) {
+//     const activeDot = document.querySelector("li.active");
+//     const activeCard = document.querySelector(".slider-card.active");
+//     const dotIndex = Array.from(indicators.children).indexOf(clickedDot);
+//     const nextActiveCard = sliderCards[dotIndex];
+//     activeDot.classList.remove("active");
+//     clickedDot.classList.add("active");
+//     activeCard.classList.remove("active");
+//     nextActiveCard.classList.add("active");
+//   }
+// });
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
 });
